6f4fe9f31b842917438c4523515b9844bd215f52
Phillip Assmann <p.assmann@nexus.gd>
2024-08-02 15:42:46 +0200

    unified transport

--- src/link.rs
+++ src/link.rs
@@ -1,24 +1,31 @@
 use std::{
     convert::identity,
     error::Error,
+    marker::PhantomData,
     sync::{mpsc, Arc},
     time::Duration,
 };
 
-use anyhow::anyhow;
+use anyhow::{anyhow, bail};
 use futures_util::SinkExt;
 use nalgebra::{Quaternion, UnitQuaternion};
 use postcard::from_bytes;
-use serde::Serialize;
 use tokio::{select, sync::watch, time::sleep};
 use tracing::error;
 use zenoh::{
     config::{AclConfigRules, Action, InterceptorFlow, Permission},
     prelude::r#async::*,
     publication::Publisher,
+    subscriber::FlumeSubscriber,
 };
 
-use argus_common::{ControlRequest, ControlResponse, GlobalPosition, LocalPosition, MissionNode};
+use argus_common::{
+    interface::{
+        IControlRequest, IControlResponse, IGlobalPosition, ILocalPosition, IMissionStep,
+        IMissionUpdate, IYaw, Interface,
+    },
+    ControlRequest, ControlResponse, GlobalPosition, LocalPosition, MissionNode,
+};
 
 use crate::{
     topics::Subscribers,
@@ -41,32 +48,21 @@ impl ArgusLink {
         machine: String,
     ) -> Self {
         tokio::spawn(async move {
-            let t = |o: &str| format!("{machine}/{o}");
+            let m = machine;
             let mut config = config::default();
 
             Self::set_acl(&mut config)?;
 
             let session = Arc::new(zenoh::open(config).res().await?);
-            let global_pos_pub = session
-                .declare_publisher(t("global_position"))
-                .res()
-                .await?;
-            let local_pos_pub = session.declare_publisher(t("local_position")).res().await?;
-            let yaw_pub = session.declare_publisher(t("yaw")).res().await?;
-            let step_pub = session.declare_publisher(t("mission/step")).res().await?;
-            let mut control_pub = session.declare_publisher(t("control/out")).res().await?;
 
-            let mission_sub = session
-                .declare_subscriber(t("mission/update"))
-                .reliable()
-                .res()
-                .await?;
+            let global_pos_pub: Pub<IGlobalPosition> = session.publisher(&m).await?;
+            let local_pos_pub: Pub<ILocalPosition> = session.publisher(&m).await?;
+            let yaw_pub: Pub<IYaw> = session.publisher(&m).await?;
+            let step_pub: Pub<IMissionStep> = session.publisher(&m).await?;
+            let mut control_pub: Pub<IControlResponse> = session.publisher(&m).await?;
 
-            let control_sub = session
-                .declare_subscriber(t("control/in"))
-                .reliable()
-                .res()
-                .await?;
+            let mission_sub: Sub<IMissionUpdate> = session.subscriber(&m).await?;
+            let control_sub: Sub<IControlRequest> = session.subscriber(&m).await?;
 
             let global_pos = subs.global_position.subscribe();
             let local_pos = subs.local_position.subscribe();
@@ -88,39 +84,25 @@ impl ArgusLink {
 
             loop {
                 select! {
-                    mission = mission_sub.recv_async() => {
+                    mission = mission_sub.pull() => {
                         match mission {
                             Ok(mission) => {
-                                match from_bytes(&mission.value.payload.contiguous()) {
-                                    Ok(plan) => {
-                                        mission_in.send(plan).unwrap();
-                                    },
-                                    Err(e) => {
-                                        error!("Failed to deserialize {e}");
-                                    }
-                                }
+                                mission_in.send(mission).unwrap();
                             },
                             Err(e) => println!("{e}"),
                         }
                     },
-                    control = control_sub.recv_async() => {
+                    control = control_sub.pull() => {
                         match control {
                             Ok(control) => {
-                                match from_bytes::<ControlRequest>(&control.value.payload.contiguous()) {
-                                    Ok(control) => {
-                                        let _ = control_link.0.send(control);
-                                    },
-                                    Err(e) => {
-                                        error!("Failed to deserialize {e}");
-                                    }
-                                }
+                                let _ = control_link.0.send(control);
                             },
                             Err(e) => println!("{e}"),
                         }
                     },
                     Some(msg) = control_link.1.recv() => {
                         let data = postcard::to_allocvec(&msg).unwrap();
-                        if let Err(e) = control_pub.send(data).await {
+                        if let Err(e) = control_pub.publisher.send(data).await {
                             error!("{e}")
                         }
                     }
@@ -131,26 +113,6 @@ impl ArgusLink {
             Ok::<_, ZenohError>(())
         });
         ArgusLink
-    }
-
-    fn bridge<T, U, F>(mut rcv: watch::Receiver<T>, mut publisher: Publisher<'static>, mut map: F)
-    where
-        F: FnMut(T) -> U + Send + 'static,
-        T: ToOwned<Owned = T> + Send + Sync + 'static,
-        U: Serialize + Send + Sync + 'static,
-    {
-        tokio::spawn(async move {
-            loop {
-                if let Ok(_) = rcv.changed().await {
-                    let update = map(rcv.borrow().to_owned());
-                    let data = postcard::to_allocvec(&update).unwrap();
-                    if let Err(e) = publisher.send(data).await {
-                        error!("{e}")
-                    }
-                }
-                sleep(Duration::from_millis(15)).await;
-            }
-        });
     }
 
     fn set_acl(config: &mut Config) -> Result<(), anyhow::Error> {
@@ -179,5 +141,83 @@ impl ArgusLink {
             .set_enabled(true)
             .map_err(|e| anyhow!("{e:?}"))?;
         Ok(())
+    }
+
+    fn bridge<I, U, F>(mut rcv: watch::Receiver<U>, mut publisher: Pub<I>, mut map: F)
+    where
+        I: Interface,
+        I::Message: Clone + Send + Sync + 'static,
+        U: Clone + Send + Sync + 'static,
+        F: FnMut(U) -> I::Message + Send + 'static,
+    {
+        tokio::spawn(async move {
+            loop {
+                if let Ok(_) = rcv.changed().await {
+                    let update = map(rcv.borrow().to_owned());
+                    let data = postcard::to_allocvec(&update).unwrap();
+                    if let Err(e) = publisher.publisher.send(data).await {
+                        error!("{e}")
+                    }
+                }
+                sleep(Duration::from_millis(15)).await;
+            }
+        });
+    }
+}
+
+pub struct Pub<I> {
+    publisher: Publisher<'static>,
+    _phantom: PhantomData<I>,
+}
+
+pub struct Sub<I> {
+    subscriber: FlumeSubscriber<'static>,
+    _phantom: PhantomData<I>,
+}
+
+impl<I: Interface> Sub<I> {
+    pub async fn pull(&self) -> anyhow::Result<I::Message> {
+        let msg = self.subscriber.recv_async().await;
+        match msg {
+            Ok(msg) => match from_bytes(&msg.value.payload.contiguous()) {
+                Ok(msg_c) => return Ok(msg_c),
+                Err(e) => {
+                    bail!("Failed to deserialize {e}");
+                }
+            },
+            Err(e) => bail!("{e}"),
+        };
+    }
+}
+
+trait MakeInterface {
+    async fn publisher<I: Interface>(&self, m: &str) -> anyhow::Result<Pub<I>>;
+    async fn subscriber<I: Interface>(&self, m: &str) -> anyhow::Result<Sub<I>>;
+}
+
+impl MakeInterface for Arc<Session> {
+    async fn publisher<I: Interface>(&self, m: &str) -> anyhow::Result<Pub<I>> {
+        let res = self
+            .declare_publisher(format!("{m}/{}", I::topic()))
+            .res()
+            .await
+            .map_err(|e| anyhow!("{e:?}"))?;
+        Ok(Pub {
+            publisher: res,
+            _phantom: PhantomData,
+        })
+    }
+    async fn subscriber<I: Interface>(&self, m: &str) -> anyhow::Result<Sub<I>> {
+        let sub = self
+            .declare_subscriber(format!("{m}/{}", I::topic()))
+            .reliable()
+            .res()
+            .await
+            .map_err(|e| anyhow!("{e:?}"))?;
+
+        Ok(Sub {
+            subscriber: sub,
+            _phantom: PhantomData,
+        })
     }
 }
